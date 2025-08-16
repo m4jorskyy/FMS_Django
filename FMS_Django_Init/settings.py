5e59e5e29a8ed366da0712c3375b359063ccb26c
m4jorskyy <igor.suchodolski@student.put.poznan.pl>
2025-08-16 23:41:07 +0200

    deployment check v2

--- FMS_Django_Init/settings.py
+++ FMS_Django_Init/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'https://fms-django.onrender.com/'
+    'fms-django.onrender.com'
 ]
 
 # Application definition
