0b19e3905e352212eb3921c70b0a2f279c9634d1
m4jorskyy <igor.suchodolski@student.put.poznan.pl>
2025-08-16 22:56:57 +0200

    deploying v4

--- build.sh
+++ build.sh
@@ -3,8 +3,12 @@ set -o errexit
 
 pip install -r requirements.txt
 
+python manage.py makemigrations FMS_Django_App
+python manage.py makemigrations
+
 python manage.py migrate contenttypes
 python manage.py migrate auth
 python manage.py migrate FMS_Django_App
+python manage.py migrate
 
 python manage.py collectstatic --no-input
